a268046ac38c4fd9c6b74502d4fe619a42dac530
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-01 16:11:59 +0900

    get 방식 일때 Valid 처리하기

--- validation1/src/main/java/com/example/validation1/controller/ApiController.java
+++ validation1/src/main/java/com/example/validation1/controller/ApiController.java
@@ -1,21 +1,41 @@
 package com.example.validation1.controller;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.validation1.dto.User;
 
+// post, get 둘다 적용
 @RestController
 @RequestMapping("/api")
+
+// 겟방식일때만 처리 ( 안쓰면 적용안됨. )
+@Validated
 public class ApiController {
+	
+	// get 방식 일때 Valid 처리하기 
+	
+	@GetMapping("/user")
+	public User user(@Size(min = 2) @RequestParam String name, @NotNull @Min(1) @RequestParam Integer age) {	// int는 notnull 안먹음
+		User user = new User();
+		user.setName(name);
+		user.setAge(age);
+		return user;
+	}
 
 	// post 방식 처리에 대함 @Valid 확인 ( 반드시 @Valid 를 지정해 주어야 한다. )
 	
