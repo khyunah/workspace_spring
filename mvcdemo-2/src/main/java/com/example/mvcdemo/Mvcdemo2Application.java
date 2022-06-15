3bd061b3a9284d896bd390f633d087f630c87755
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-15 17:29:52 +0900

    mustache사용해보기

---
+++ mvcdemo-2/src/main/java/com/example/mvcdemo/Mvcdemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.mvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvcdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvcdemo2Application.class, args);
+	}
+
+}
---
+++ mvcdemo-2/src/test/java/com/example/mvcdemo/Mvcdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvcdemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
