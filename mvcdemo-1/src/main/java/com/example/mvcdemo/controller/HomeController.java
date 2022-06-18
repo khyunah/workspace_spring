5d4355ee965a62e6e4dc3b7e80d6914ed3f980af
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-19 00:33:42 +0900

    mustache 사용 {{}}, {{{}}}

--- mvcdemo-1/src/main/java/com/example/mvcdemo/controller/HomeController.java
+++ mvcdemo-1/src/main/java/com/example/mvcdemo/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.mvcdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
@@ -11,5 +12,11 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("name", "김현아");
 		return "home";
+	}
+	
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("hi", "<h1>안녕~!</h1>");
+		return "hello";
 	}
 }
